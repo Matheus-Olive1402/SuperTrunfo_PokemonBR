3c0223108e73e81d8ab8befe6f75b94d34506a23
matheus oliveira <matheus.olive1402@hotmail.com>
2021-10-11 16:12:42 -0300

    correção de pontuação

--- src/script.js
+++ src/script.js
@@ -4,7 +4,7 @@ var Bulbassalto = {
   atributos: {
     Porte_do_homem: 5,
     Colete: 7,
-    BO: 5
+    BO: 6
   }
 };
 var Charmano = {
@@ -13,7 +13,7 @@ var Charmano = {
   atributos: {
     Porte_do_homem: 7,
     Colete: 6,
-    BO: 5
+    BO: 6
   }
 };
 var Squardocrime = {
